161ce3923362132ba94522b92fe061452f7540f3
pearisgreen <gerrit.proessl@gmail.com>
2019-01-05 21:47:57 +0100

    update

--- plisp.c
+++ plisp.c
@@ -1,6 +1,10 @@
 extern void *malloc(size_t);
 extern void free(void*);
 extern printf;
+extern int getchar();
+extern void assert(int);
+extern int strcmp(char*,char*);
+extern int strcpy(char*,char*);
 
 enum {
   TINT = 1,
@@ -17,9 +21,9 @@ enum {
 };
 
 struct obj_t;
-typedef struct obj_t *primative(void *root, struct obj_t **env, struct obj **args);
+typedef struct obj_t *primative(struct obj_t *root, struct obj_t **env, struct obj **args);
 
-struct obj_t {
+typedef struct obj_t {
   int type;
   unsigned int size;
   unsigned char gc_r; // used by gc
@@ -38,17 +42,17 @@ struct obj_t {
       struct obj_t *up;
     };
   };
-};
+} obj_t;
 
 
-//Constanst
-static struct obj_t *True    = &(struct obj_t) { TTRUE };
-static struct obj_t *Nil     = &(struct obj_t) { TNIL };
-static struct obj_t *Dot     = &(struct obj_t) { TDOT };
-static struct obj_t *Cparen  = &(struct obj_t) { TCPAREN };
+//Constants
+static obj_t *True    = &(obj_t) { TTRUE };
+static obj_t *Nil     = &(obj_t) { TNIL };
+static obj_t *Dot     = &(obj_t) { TDOT };
+static obj_t *Cparen  = &(obj_t) { TCPAREN };
 
 //list containing all symbols
-static struct obj_t *symbols;
+static obj_t *symbols;
 
 void error(char *emsg) {
   printf("error: %s\n", emsg);
@@ -60,14 +64,14 @@ void error(char *emsg) {
 
 #define MAX_MEM 8096
 
-struct obj_list_t {
-  struct obj_t *obj;
+typedef struct obj_list_t {
+  obj_t *obj;
   struct obj_list_t *next;
-};
-struct obj_list_t *obj_list = 0;
+} obj_list_t;
+obj_list_t *obj_list = 0;
 
-void obj_list_add(struct obj_t *obj) {
-  struct obj_list_t *temp = malloc(sizeof(struct obj_list_t));
+void obj_list_add(obj_t *obj) {
+  obj_list_t *temp = malloc(sizeof(obj_list_t));
   if(!temp) {
     error("allocation failed");
     exit(0);
@@ -79,13 +83,13 @@ void obj_list_add(struct obj_t *obj) {
 
 unsigned int mem_used = 0;
 
-static void gc(void *root) {
+static void gc(obj_t *root) {
   //walk root and set flag
 
 
   //free all objs that have flag set
-  struct obj_list_t *f_entry = 0;
-  for(struct obj_list_t **entry = &obj_list; *entry;) {
+  obj_list_t *f_entry = 0;
+  for(obj_list_t **entry = &obj_list; *entry;) {
     if(!*entry->obj->gc_r) {
       free(*entry->obj);
       f_entry = *entry;
@@ -97,7 +101,7 @@ static void gc(void *root) {
   }
 }
 
-static obj_t *alloc(void *root, int type, size_t size) {
+static obj_t *alloc(obj_t *root, int type, size_t size) {
   size += offsetof(obj_t, value);
 
   if(size + mem_used >= MEM_MAX)
@@ -108,7 +112,7 @@ static obj_t *alloc(void *root, int type, size_t size) {
     exit(0);
   }
 
-  struct obj_t *obj = malloc(size);
+  obj_t *obj = malloc(size);
   if(!obj) {
     error("allocation failed");
     exit(0);
@@ -122,6 +126,290 @@ static obj_t *alloc(void *root, int type, size_t size) {
   
   return obj;
 }
+
+#define ROOT_END  ((void*)-1)
+
+#define ADD_ROOT(size)                        \
+  obj_t *root_ADD_ROOT_[size+2]               \
+  root_ADD_ROOT_[0] = root;                   \
+  for(int i = 1; i <= size; i++)              \
+    root_ADD_ROOT_[i] = 0;                    \
+  root_ADD_ROOT_[size+1] = ROOT_END;          \
+  root = root_ADD_ROOT_;
+
+#define DEFINE1(var1)                         \
+  ADD_ROOT(1)                                 \
+  obj_t *var1 = (obj_t*)(root_ADD_ROOT_ + 1)  \
+
+#define DEFINE2(var1, var2)                   \
+  ADD_ROOT(2)                                 \
+  obj_t *var1 = (obj_t*)(root_ADD_ROOT_ + 1)  \
+  obj_t *var2 = (obj_t*)(root_ADD_ROOT_ + 2)  \
+    
+#define DEFINE3(var1, var2, var3)             \
+  ADD_ROOT(3)                                 \
+  obj_t *var1 = (obj_t*)(root_ADD_ROOT_ + 1)  \
+  obj_t *var2 = (obj_t*)(root_ADD_ROOT_ + 2)  \
+  obj_t *var3 = (obj_t*)(root_ADD_ROOT_ + 3)  \
+ 
+#define DEFINE4(var1, var2, var3, var4)       \
+  ADD_ROOT(4)                                 \
+  obj_t *var1 = (obj_t*)(root_ADD_ROOT_ + 1)  \
+  obj_t *var2 = (obj_t*)(root_ADD_ROOT_ + 2)  \
+  obj_t *var3 = (obj_t*)(root_ADD_ROOT_ + 3)  \
+  obj_t *var4 = (obj_t*)(root_ADD_ROOT_ + 4)  \
+ 
+//---------------------------------------- 
+// Constructors
+//---------------------------------------- 
+
+static obj_t *make_int(obj_t *root, int value) {
+  obj_t *obj = alloc(root, TINT, sizeof(int));
+  obj->value = value;
+  return obj;
+}
+
+static obj_t *cons(obj_t *root, obj_t *var, obj_t *cdr) {
+  obj_t *obj = cell = alloc(root, TCELL, sizeof(obj_t*) * 2);
+  obj->car = car;
+  obj->cdr = cdr;
+  return obj;
+}
+
+static obj_t *make_primative(obj_t *root, primative *fn) {
+  obj_t *obj = alloc(root, TPRIMATIVE, sizeof(primative*));
+  obj->fn = fn;
+  return obj;
+}
+
+static obj_t *make_function(obj_t *root, obj_t *env, int type, obj_t *params, obj_t *body) {
+  assert(type == TFUNCTION || type == TMACRO);  
+  obj_t *obj = alloc(root, type, sizeof(obj_t*) * 3);
+  obj->params = params;
+  obj->body = body;
+  obj->env = env;
+  return obj;
+}
+
+static obj_t *make_env(obj_t *root, obj_t *vars, obj_t *up) {
+  obj_t *obj = alloc(root, TENV, sizeof(obj_t*) * 2);
+  obj->vars = vars;
+  obj->up = up;
+  return obj;
+}
+
+// ((x . y) . a)
+static obj_t *acons(obj_t *root, obj_t *x, obj_t *y, obj_t *a) {
+  DEFINE1(cell);
+  cell = cons(root, x, y);
+  return cons(root, cell, a);
+}
+
+//---------------------------------------- 
+// PARSER
+//---------------------------------------- 
+
+#define SYMBOL_MAX_LEN 200
+const char symbol_chars[] = "~!@#$&^&*-_=+:/?<>";
+
+static obj_t *read_exp(obj_t *root);
+
+static int peek(void) {
+  int c = getchar();
+  ungetc(c, stdin);
+  return c;
+}
+
+static obj_t *reverse(obj_t *p) {
+  obj_t *ret = Nil;
+  while(p != Nil) {
+    obj_t *head = p;
+    p = p->cdr;
+    head->cdr = ret;
+    ret = head;
+  }
+  return ret;
+}
+
+static void skip_line(void) {
+  for(;;) {
+    int c = getchar();
+    if(c == EOF ||c == '\n')
+      return;
+    if(c == '\r') {
+      if(peek() == '\n')
+        getchar();
+      return;
+    }
+  }
+}
+
+// '(' has already been read
+static obj_t *read_list(obj_t *root) {
+  DEFINE3(obj, head, last);
+  head = Nil;
+  for(;;) {
+    obj = read_exp(root);
+    if(!obj)
+      error("unclosed paranthesis");
+    if(obj == Cparen)
+      return reverse(head);
+    if(obj == Dot) {
+      last = read_exp(root);
+      if(read_exp(root) != Cparen)
+        error("close paranthesis expected after dot");
+      obj_t *ret = reverse(head);
+      head->cdr = last;
+      return last;
+    }
+    head = cons(root, obj, head);
+  }
+}
+
+
+// returns symbol if name is already present
+static obj_t *intern(obj_t *root, char *name) {
+  for(obj_t *p = symbols; p != Nil; p = p->cdr)
+    if(!strcmp(name, p->car->name)) 
+      return p->car;
+  DEFINE1(sym);
+  sym = make_symbol(root, name);
+  symbols = cons(root, sym, &symbols); 
+  return sym;
+}
+
+static obj_t *read_quote(obj_t *root) {
+  DEFINE2(sym, tmp);
+  sym = intern(root, "quote");
+  tmp = read_exp(root);
+  tmp = cons(root, tmp, &Nil);
+  tmp = cons(root, sym, rmp);
+  return tmp;
+}
+
+static int read_number(int val) {
+  while(isdigit(peek()))
+    val = val * 10 + (getchar() - '0');
+  return val;
+}
+
+static obj_t *read_symbol(void *root, char c) {
+  char buf[SYMBOL_MAX_LEN + 1];
+  buf[0] = c;
+  int len = 1;
+  while(isalnum(peek()) || strchr(symbol_chars, peek())) {
+    if(SYMBOL_MAX_LEN <= len)
+      error("symbol name too long");
+    buf[len++] = getchar();
+  }
+  buf[len] = 0;
+  return intern(root, buf);
+}
+
+static obj_t *read_exp(void *root) {
+  for(;;) {
+    int c = getchar();
+    if(c == ' ' || c == '\n' || c == '\r' || c == '\t') 
+      continue;
+    if(c == EOF)
+      return 0;
+    if(c == ';') {
+      skip_line();
+      continue;
+    }
+    if(c == '(')
+      return read_list(root);
+    if(c == ')')
+      return Cparen;
+    if(c == '.')
+      return Dot;
+    if(c == '\'')
+      return read_quote(root);
+    if(isdigit(c))
+      return make_int(root, read_number(c - '0'));
+    if(c == '-' && isdigit(peek()))
+      return make_int(root, -read_number(0));
+    if(isalpha(c) || strchr(symbol_chars, c))
+      return read_symbol(root, c);
+    errot("unable to handle char"); //TODO: print c
+  }
+}
+
+static void print(obj_t *obj) {
+  switch(obj->type) {
+    case TCELL:
+      print("(");
+      for(;;) {
+        print(obj->car);
+        if(obj->cdr == Nil)
+          break;
+        if(obj->cdr->type != TCELL) {
+          printf(" . ");
+          print(obj->cdr);
+          break;
+        }
+        printf(" ");
+        return;
+      }
+      printf(")");
+      return;
+
+#define CASE(type, ...)       \
+  case type:                  \
+      printf(__VA_ARGS__);    \
+      return
+  CASE(TINT, "%d", obj->value);
+  CASE(TSYMBOL, "%s", obj->name);
+  CASE(TPRIMATIVE, "<primative>");
+  CASE(TFUNCTION, "<function>");
+  CASE(TMACRO, "<macro>");
+  CASE(TMOVED, "<moved>");
+  CASE(TTRUE, "t");
+  case(TNIL, "()");
+#undef CASE
+  
+  default:
+    error("compiler error: unknown tag type");
+  }
+}
+
+static int length(obj_t *list) {
+  int len = 0;
+  for(; list->type == TCELL; list = list->cdr)
+    len++;
+  return list == Nil ? len : -1;
+}
+
+//---------------------------------------- 
+// EVALUATOR
+//---------------------------------------- 
+
+static obj_t *eval(obj_t *root, obj_t *env, obj_t *obj);
+
+static void add_variable(obj_t *root, obj_t *env, obj_t *sym, obj_t *val) {
+  DEFINE2(vars, *tmp);
+  vars = env->vars;
+  tmp = acons(root, sym, val, vars);
+  env->vars = tmp;
+}
+
+static obj_t *push_env(obj_t *root, obj_t *env, obj_t *vars, obj_t *vals) {
+  DEFINE3(map, sym, val);
+  map = Nil;
+  for(; vars->type == TCEL; vars = vars->cdr, vals = vals->cdr) {
+    if(vals->type != TCELL) 
+      error("cannot apply function: number of argument does not match");
+    sym = vars->car;
+    val = vals->car;
+    map = acons(root, sym, val, map);
+  }
+  if(vars != Nil)
+    map = acons(root, vars, vals, map);
+  return make_env(root, map, env);
+}
+
+//evaluates the list elements from the head and returns the last value
+
 
 
 
